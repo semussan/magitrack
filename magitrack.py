243533683b93c82ee51270ca5b938cff86f4a513
semussan <semussan@gmail.com>
2026-09-20 16:57:18 -0400

    whitespace error

--- magitrack.py
+++ magitrack.py
@@ -65,7 +65,7 @@ def remove_game_result(i):
 
 
 def add_game_result():
-	clean_text = state.new_item_text.replace(" ","").strip()
+    clean_text = state.new_item_text.replace(" ","").strip()
     state.game_results.append(GameResult(text=clean_text))
     state.new_item_text = ""
     save_data(state.game_results)
